af4b294796810c490a6eae7ca0ab8297f4ae972e
pubuzhixing8 <pubuzhixing@gmail.com>
2022-06-12 20:21:19 +0800

    feat(layouts): fix non overlap tree layout

--- projects/tiny-tree-layouts/src/hierarchy/wrap.ts
+++ projects/tiny-tree-layouts/src/hierarchy/wrap.ts
@@ -6,6 +6,6 @@ export const wrap = (root: Node, isHorizontal: boolean) => {
     root.children.forEach(child => {
         children.push(wrap(child, isHorizontal));
     });
-    if (isHorizontal) return new Tree(root.height, root.width, root.x, children);
-    return new Tree(root.width, root.height, root.y, children);
+    if (isHorizontal) return new Tree(root.height, root.width, root.x, children, root.origin);
+    return new Tree(root.width, root.height, root.y, children, root.origin);
 };
